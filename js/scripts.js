327bdf291dc9bd23452ed03236ec907f6b006ddd
Mikerophonic <micgcon@gmail.com>
2023-06-12 20:22:41 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+const form = document.querySelector("form");
+form.onsubmit = function(event) {
+  event.preventDefault();
+
+let oldEnough = document.getElementById("old-enough");
+oldEnough.setAttribute("class", "hidden");
+let minor = document.getElementById("minor");
+minor.setAttribute("class", "hidden");
+const age = parseInt(document.querySelector("input#age").value);
+if (age >= 18) {
+  oldEnough.removeAttribute("class");
+} else {
+  minor.removeAttribute("class");
+ }
+};
+};
